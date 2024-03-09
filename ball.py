0a70dd1d6b93dcfdf4ab1ef20d864ef60d77e38b
LeoDodik <119758958+LeoDodik@users.noreply.github.com>
2024-03-09 12:36:46 +0100

    Update ball.py

--- ball.py
+++ ball.py
@@ -1,16 +1,16 @@
 from turtle import Turtle
-
+from paddle import Paddle
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("circle")  # Set the shape to a circle
-        self.shapesize(stretch_wid=2, stretch_len=2)  # Set the width and height
+        self.shapesize(stretch_wid=1, stretch_len=1)  # Set the width and height
         self.penup()  # Lift the pen to avoid drawing lines
         self.goto(0, 0)  # Set initial position
         self.color("white")
         self.x_move = 10
         self.y_move = 10
-
+        self.move_speed = 0.1
     def move(self):
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
@@ -20,5 +20,16 @@ class Ball(Turtle):
         if new_y > 290 or new_y < -290:
             self.bounce_y()
 
+    def increase_speed(self):
+        self.speed("fastest")
+
     def bounce_y(self):
         self.y_move *= -1
+        self.move_speed *= 0.9
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+    def restart(self):
+        self.goto(0,0)
+        self.bounce_x()
